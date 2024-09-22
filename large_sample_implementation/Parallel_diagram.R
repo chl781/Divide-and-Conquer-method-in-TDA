30257ff67dcc57631ba3a0cc44f9cd6087e6f6c7
Chenghui Li <52631717+chl781@users.noreply.github.com>
2024-09-21 19:41:50 -0500

    Anonymize

--- large_sample_implementation/Parallel_diagram.R
+++ large_sample_implementation/Parallel_diagram.R
@@ -1,20 +1,3 @@
-# Simulation_Only uniform distribution on Sphere. 
-# That is the underlying manifold can only be perfect circles
-# or perfect spheres. Then this problem can be reduced based on 1 method of calculating
-# the death time.
-
-# Consider generating some data from this kind of manifolds and do the analysis
-# This is parallel case used in CHTC.
-
-# Only generate the data following a wrappednormal distribution.
-
-# So, the only change should be the distribution of the data. However, this should not 
-# play a role.
-
-# Number of data also play a role.
-
-# In this file, only the data generation method is modified.
-
 (args = commandArgs(trailingOnly=TRUE))
 (j <- args[1])
 j2 <- as.numeric(j)
@@ -22,10 +5,6 @@ j2 <- j2 + 1
 
 j <- args[2]
 j1 <- as.numeric(j)
-
-###### Derive a MSE data.frame but for only one iteration.
-
-# Not use the same random seed!
 
 library(pacman)
 p_load("dplyr","plotrix","spatstat","TDA","hitandrun","functional","Rfast","plotly","viridis","plot3D","ggplot2")
